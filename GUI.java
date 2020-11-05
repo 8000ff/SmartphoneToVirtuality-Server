5860dded484463983bb147e51df9d6478d3e3992
MrRedbloX <charles.cogoluegnes@gmail.com>
2020-11-05 08:30:22 +0100

    Adding javadoc

--- GUI.java
+++ GUI.java
@@ -13,6 +13,11 @@ import javafx.scene.layout.GridPane;
 
 import javafx.stage.Stage;
 
+/**
+ * Graphical user interface for the UDP server.
+ * Reflects the data of the sensor given to the server in real time.
+ * @author LE BERRE Samuel and COGOLUEGNES Charles
+ */
 public class GUI extends Application{
 
     private static Label ups = new Label("ups");
@@ -23,22 +28,17 @@ public class GUI extends Application{
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Smartphone To Virtuality");
-
         GridPane grid = new GridPane();
 
-        //Ajout Label Ups
         GridPane.setConstraints(ups, 0, 0);
         grid.getChildren().add(ups);
 
-        //Ajout Label Accelerometres
         GridPane.setConstraints(accelerometer, 0, 1);
         grid.getChildren().add(accelerometer);
 
-        //Ajout Label Gyroscope
         GridPane.setConstraints(gyroscope, 0, 2);
         grid.getChildren().add(gyroscope);
 
-        //Ajout Label proximité
         GridPane.setConstraints(proximity, 0, 3);
         grid.getChildren().add(proximity);
 
@@ -46,18 +46,38 @@ public class GUI extends Application{
         primaryStage.show();
     }
 
+    /**
+     * Set the label for the number of updates per second.
+     * @param val_ a int that represents the number of ups.
+     */
     public static void setUps(int val_){
       Platform.runLater(() -> ups.setText("Update/sec : " + val_));
     }
-
+    
+    /**
+     * Set the label for the accelerometer sensor.
+     * @param x the x coord.
+     * @param y the y coord.
+     * @param z the z coord.
+     */
     public static void setAccelerometer(float x_, float y_, float z_){
       Platform.runLater(() -> accelerometer.setText("Accelerometer : x = " + x_ + " , y = " + y_ + " , z = " + z_));
     }
 
+    /**
+     * Set the label for the gyroscope sensor.
+     * @param x the x coord.
+     * @param y the y coord.
+     * @param z the z coord.
+     */
     public static void setGyroscope(float x_, float y_, float z_){
       Platform.runLater(() -> gyroscope.setText("Gyroscope : x = " + x_ + " , y = " + y_ + " , z = " + z_));
     }
 
+    /**
+     * Set the label for the proximity sensor.
+     * @param val_ the value of the proximity.
+     */
     public static void setProximity(float val_){
       Platform.runLater(() -> proximity.setText("Proximity : " + val_));
     }
